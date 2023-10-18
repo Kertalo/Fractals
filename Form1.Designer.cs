6c3aef66d9aa65f2f76974982c75ac63290560f9
kertalo <koksharov-kir@yandex.ru>
2023-10-18 09:28:29 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Hard_Lab_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox = new PictureBox();
+            ButtonRun = new Button();
+            textBox = new TextBox();
+            checkBox = new CheckBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            pictureBox.BackColor = Color.DarkSlateGray;
+            pictureBox.Location = new Point(274, 12);
+            pictureBox.Name = "pictureBox";
+            pictureBox.Size = new Size(720, 697);
+            pictureBox.TabIndex = 0;
+            pictureBox.TabStop = false;
+            // 
+            // ButtonRun
+            // 
+            ButtonRun.BackColor = Color.DimGray;
+            ButtonRun.BackgroundImageLayout = ImageLayout.None;
+            ButtonRun.FlatStyle = FlatStyle.Popup;
+            ButtonRun.Font = new Font("Arial Black", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            ButtonRun.ForeColor = Color.White;
+            ButtonRun.Location = new Point(12, 210);
+            ButtonRun.Name = "ButtonRun";
+            ButtonRun.Size = new Size(94, 29);
+            ButtonRun.TabIndex = 3;
+            ButtonRun.Text = "Run";
+            ButtonRun.UseVisualStyleBackColor = false;
+            ButtonRun.Click += ButtonRunClick;
+            // 
+            // textBox
+            // 
+            textBox.BackColor = Color.DimGray;
+            textBox.BorderStyle = BorderStyle.None;
+            textBox.Font = new Font("Arial Black", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox.ForeColor = Color.White;
+            textBox.Location = new Point(12, 12);
+            textBox.Multiline = true;
+            textBox.Name = "textBox";
+            textBox.RightToLeft = RightToLeft.No;
+            textBox.Size = new Size(256, 192);
+            textBox.TabIndex = 4;
+            // 
+            // checkBox
+            // 
+            checkBox.AutoSize = true;
+            checkBox.CheckAlign = ContentAlignment.MiddleRight;
+            checkBox.Location = new Point(112, 213);
+            checkBox.Name = "checkBox";
+            checkBox.Size = new Size(78, 24);
+            checkBox.TabIndex = 5;
+            checkBox.Text = "Is tree?";
+            checkBox.UseVisualStyleBackColor = true;
+            checkBox.CheckedChanged += CheckBoxCheckedChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.DarkSlateGray;
+            ClientSize = new Size(1006, 721);
+            Controls.Add(checkBox);
+            Controls.Add(textBox);
+            Controls.Add(ButtonRun);
+            Controls.Add(pictureBox);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox;
+        private Button ButtonRun;
+        private TextBox textBox;
+        private CheckBox checkBox;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,277 @@
+﻿using System.CodeDom.Compiler;
+using System.Drawing;
+using System.Globalization;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Hard_Lab_5
+{
+    public partial class Form1 : Form
+    {
+        Bitmap map;
+        Graphics graphics;
+        bool isTree;
+
+        struct position
+        {
+            public float x, y, angle;
+            public position(float x, float y, float angle)
+            {
+                this.x = x;
+                this.y = y;
+                this.angle = angle;
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            map = new Bitmap(pictureBox.Width, pictureBox.Height);
+            graphics = Graphics.FromImage(map);
+            isTree = checkBox.Checked;
+        }
+
+        private void ButtonRunClick(object sender, EventArgs e)
+        {
+            var rand = new Random();
+
+            graphics.Clear(pictureBox.BackColor);
+
+            var pattern = @"\s*(?<atom>\d+)\s+(?<angle>[-+]?\d+\.?\d+)\s+" +
+                @"(?<startVector>\S+)\s*";
+
+            string[] allText = textBox.Text.Split('\n');
+            var match = Regex.Match(allText[0], pattern);
+
+            if (!match.Success)
+                return;
+
+            int n = Int32.Parse(match.Groups[1].Value);
+            string start = match.Groups[3].Value;
+            float addAngle = float.Parse(match.Groups[2].Value, CultureInfo.InvariantCulture);
+
+            Dictionary<char, string> rules = new Dictionary<char, string>();
+            string[] sRules = allText[1..];
+            foreach (string sRule in sRules)
+            {
+                pattern = @"\s*(?<atom>.)\s*->\s*(?<rule>.+)\s*\r?";
+                match = Regex.Match(sRule, pattern);
+                if (match.Success)
+                    rules[char.Parse(match.Groups[1].Value)] =
+                        match.Groups[2].Value;
+            }
+            if (rules.Count == 0)
+                return;
+
+            string temp = "";
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < start.Length; j++)
+                {
+                    if (rules.ContainsKey(start[j]))
+                        temp += rules[start[j]];
+                    else
+                        temp += start[j];
+                }
+                start = temp;
+                temp = "";
+            }
+
+            float startX = 1000;
+            float startY = 1000;
+            float minX = 0;
+            float maxX = 0;
+            float minY = 0;
+            float maxY = 0;
+
+            float x = startX;
+            float y = startY;
+
+            float addMove = 1;
+            float angle = 90;
+
+            Stack<position> stack = new Stack<position>();
+            Queue<float> angles = new Queue<float>();
+
+            bool isRandom = false;
+
+            for (int i = 0; i < start.Length; i++)
+            {
+                if (start[i] == 'F')
+                {
+                    float nextX = x + (float)Math.Cos(angle * Math.PI / 180) * addMove;
+                    float nextY = y - (float)Math.Sin(angle * Math.PI / 180) * addMove;
+                    x = nextX;
+                    y = nextY;
+
+                    if (startX - minX > x)
+                        minX = startX - x;
+                    if (startY - minY > y)
+                        minY = startY - y;
+                    if (startX + maxX < x)
+                        maxX = x - startX;
+                    if (startY + maxY < y)
+                        maxY = y - startY;
+                }
+                else if (start[i] == '+')
+                {
+                    if (isRandom)
+                    {
+                        var randomAngle = (float)rand.NextDouble() * addAngle;
+                        angles.Enqueue(randomAngle);
+                        angle += randomAngle;
+                    }
+                    else
+                        angle += addAngle;
+                    angle %= 360;
+                }
+                else if (start[i] == '-')
+                {
+                    if (isRandom)
+                    {
+                        var randomAngle = (float)rand.NextDouble() * addAngle;
+                        angles.Enqueue(randomAngle);
+                        angle -= randomAngle;
+                    }
+                    else
+                        angle -= addAngle;
+                    angle %= 360;
+                }
+                else if (start[i] == '[')
+                {
+                    stack.Push(new position(x, y, angle));
+                }
+                else if (start[i] == ']')
+                {
+                    if (stack.Count == 0)
+                        continue;
+                    position pos = stack.Pop();
+                    x = pos.x;
+                    y = pos.y;
+                    angle = pos.angle;
+                }
+                else if (start[i] == '@')
+                {
+                    isRandom = true;
+                }
+            }
+
+            map = new Bitmap(pictureBox.Width, pictureBox.Height);
+            graphics = Graphics.FromImage(map);
+
+            if (!isTree && ((map.Width >= map.Height && minX + maxX < minY + maxY) ||
+                (map.Width < map.Height && minX + maxX > minY + maxY)))
+            {
+                angle = 180;
+                float tempC = minX;
+                minX = minY;
+                minY = maxX;
+                maxX = maxY;
+                maxY = tempC;
+            }
+            else
+                angle = 90;
+            float newAddMove1 = map.Height / (minY + maxY);
+            float newAddMove2 = map.Width / (minX + maxX);
+            if (newAddMove1 < newAddMove2)
+                addMove = newAddMove1;
+            else
+                addMove = newAddMove2;
+
+            float width = isTree ? 20 : 2;
+            int iter = 0;
+
+            Color color = isTree ? Color.FromArgb(255, 150, 75, 0) : Color.White;
+            Pen pen;
+            pen = new Pen(color, width);
+
+            x = map.Width * minX / (minX + maxX);
+            y = map.Height * minY / (minY + maxY);
+
+            for (int i = 0; i < start.Length; i++)
+            {
+                if (start[i] == 'F')
+                {
+                    float nextX = x + (float)Math.Cos(angle * Math.PI / 180) * addMove;
+                    float nextY = y - (float)Math.Sin(angle * Math.PI / 180) * addMove;
+                    graphics.DrawLine(pen, new PointF(x, y), new PointF(nextX, nextY));
+                    x = nextX;
+                    y = nextY;
+                }
+                else if (start[i] == '+')
+                {
+                    angle += isRandom ? angles.Dequeue() : addAngle;
+                    angle %= 360;
+                }
+                else if (start[i] == '-')
+                {
+                    angle -= isRandom ? angles.Dequeue() : addAngle;
+                    angle %= 360;
+                }
+                else if (start[i] == '[')
+                {
+                    stack.Push(new position(x, y, angle));
+                    if (isTree)
+                    {
+                        iter++;
+                        float newWidth = (n - iter) * 1.0f / (n * 1.0f) * width;
+                        if (newWidth < 1)
+                            newWidth = 1;
+                        double r;
+                        double g;
+                        if (n >= iter)
+                        {
+                            r = Math.Sqrt(n * (n - iter)) / n * 150;
+                            g = 128 - Math.Sqrt(n * (n - iter)) / n * 53;
+                        }
+                        else
+                        {
+                            r = 0;
+                            g = 128;
+                        }
+                        color = Color.FromArgb((int)r, (int)g, 0);
+                        pen = new Pen(color, newWidth);
+                    }
+                }
+                else if (start[i] == ']')
+                {
+                    if (stack.Count == 0)
+                        continue;
+                    position pos = stack.Pop();
+                    x = pos.x;
+                    y = pos.y;
+                    angle = pos.angle;
+                    if (isTree)
+                    {
+                        iter--;
+                        float newWidth = (n - iter) * 1.0f / (n * 1.0f) * width;
+                        if (newWidth < 1)
+                            newWidth = 1;
+                        double r;
+                        double g;
+                        if (n >= iter)
+                        {
+                            r = Math.Sqrt(n * (n - iter)) / n * 150;
+                            g = 128 - Math.Sqrt(n * (n - iter)) / n * 53;
+                        }
+                        else
+                        {
+                            r = 0;
+                            g = 128;
+                        }
+                        color = Color.FromArgb((int)r, (int)g, 0);
+                        pen = new Pen(color, newWidth);
+                    }
+                }
+            }
+
+            pictureBox.Image = map;
+        }
+
+        private void CheckBoxCheckedChanged(object sender, EventArgs e)
+        {
+            isTree = checkBox.Checked;
+        }
+    }
+}
